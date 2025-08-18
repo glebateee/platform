43c75c2629ebceb21f499b6b0405ff519268c6ff
Me <ggg@gmail.com>
2025-08-18 17:20:33 +0300

    added route printing

--- http/handling/request_dispatch.go
+++ http/handling/request_dispatch.go
@@ -43,6 +43,16 @@ func (router *RouterComponent) ProcessRequest(ctx *pipeline.ComponentContext, ne
 	ctx.ResponseWriter.WriteHeader(http.StatusNotFound)
 }
 
+func printRoute(route Route) {
+	fmt.Printf("\n-------------\nhttpMethod: %v\n", route.httpMethod)
+	fmt.Printf("prefix: %v\n", route.prefix)
+	fmt.Printf("handlerName: %v\n", route.handlerName)
+	fmt.Printf("actionName: %v\n", route.actionName)
+	fmt.Printf("actionName: %v\n", route.expression.String())
+	fmt.Printf("handlerMethod: %v\n-------------\n", route.handlerMethod.Name)
+
+}
+
 func (router *RouterComponent) invokeHandler(route Route, rawParams []string, ctx *pipeline.ComponentContext) error {
 	paramVals, err := params.GetParametersFromRequest(ctx.Request, route.handlerMethod, rawParams)
 	if err == nil {
@@ -51,6 +61,7 @@ func (router *RouterComponent) invokeHandler(route Route, rawParams []string, ct
 		paramVals = append([]reflect.Value{structVal.Elem()}, paramVals...)
 		result := route.handlerMethod.Func.Call(paramVals)
 		io.WriteString(ctx.ResponseWriter, fmt.Sprint(result[0].Interface()))
+		printRoute(route)
 	}
 	return err
 }
